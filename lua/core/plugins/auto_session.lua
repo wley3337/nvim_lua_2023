a58bc88ece803d0f71c58a1155b8589d593876eb
Will Ley <wley3337@gmail.com>
2024-04-28 21:13:12 -0400

    Add copiolit and auto-session, fix a ruff load bug

---
+++ lua/core/plugins/auto_session.lua
@@ -0,0 +1,22 @@
+return {
+	"rmagatti/auto-session",
+	config = function()
+		local auto_session_ok, auto_session = pcall(require, "auto-session")
+		if not auto_session_ok then
+			print("Auto Session could not be installed")
+			return
+		end
+		auto_session.setup({
+			auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+			auto_session_use_git_branch = true, -- true, false, nil
+			log_level = "error",
+			-- Set mapping for searching a session.
+			-- ⚠️ This will only work if Telescope.nvim is installed
+			vim.keymap.set("n", "<Leader>ls", require("auto-session.session-lens").search_session, {
+				noremap = true,
+			}),
+		})
+	end,
+
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+}
