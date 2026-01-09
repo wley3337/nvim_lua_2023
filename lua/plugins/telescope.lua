9d426f5baaddcc2fdb3c09915d98614620ce6781
Will Ley <wley3337@gmail.com>
2026-01-09 06:39:14 -0500

    Remove duplicate command telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -163,16 +163,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, {
 			desc = "[ ] Find existing buffers",
 		})
-		vim.keymap.set("n", "<leader>/", function()
-			-- You can pass additional configuration to telescope to change theme, layout, etc.
-			require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-				winblend = 10,
-				previewer = false,
-			}))
-		end, {
-			desc = "[/] Fuzzily search in current buffer]",
-		})
-
 		vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, {
 			desc = "[F]ind [F]iles",
 		})
